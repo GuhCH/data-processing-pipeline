1e3e236461696d4f57bc3b2f68dbfbbe7adb96be
GuhCh <joegocher@gmail.com>
2022-07-27 16:24:59 +0100

    - sets up streaming consumer to consume live streaming data from kafka into spark streaming

--- streaming_consumer.py
+++ streaming_consumer.py
@@ -1,13 +1,29 @@
-from kafka import KafkaConsumer
-import json
+import findspark
+findspark.init('/home/guhch/spark/spark-3.3.0-bin-hadoop3/')
+import pyspark
+from pyspark.sql import SparkSession
+import os
+import multiprocessing
 
-streaming_consumer = KafkaConsumer(
-    bootstrap_servers="localhost:9092",    
-    value_deserializer=lambda message: json.loads(message),
-    auto_offset_reset="earliest"
-)
+os.environ['PYSPARK_SUBMIT_ARGS'] = '--packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0 streaming_consumer.py pyspark-shell'
 
-streaming_consumer.subscribe(topics='PinterestData')
+kafka_topic_name = 'PinterestData'
+kafka_bootstrap_servers = 'localhost:9092'
 
-for message in streaming_consumer:
-    print(json.dumps(message.value))
+session = SparkSession \
+        .builder \
+        .appName("Kafka") \
+        .getOrCreate()
+
+session.sparkContext.setLogLevel("ERROR")
+
+data_df = session \
+    .readStream \
+    .format('kafka') \
+    .option('kafka.bootstrap.servers', kafka_bootstrap_servers) \
+    .option('subscribe', kafka_topic_name) \
+    .option('startingOffsets', 'earliest') \
+    .load()
+
+data_df = data_df.selectExpr("CAST(value as STRING)")
+data_df.writeStream.outputMode('append').format('console').start().awaitTermination()
