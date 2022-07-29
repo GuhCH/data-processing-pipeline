24ebd1b43977e2ce3e00de5acdf31430b679c1c7
GuhCh <joegocher@gmail.com>
2022-07-29 16:02:37 +0100

    - some cleaning operations are now performed on streaming data

--- streaming_consumer.py
+++ streaming_consumer.py
@@ -1,7 +1,10 @@
+from curses import window
 import findspark
 findspark.init('/home/guhch/spark/spark-3.3.0-bin-hadoop3/')
 import pyspark
 from pyspark.sql import SparkSession
+from pyspark.sql.types import *
+from pyspark.sql.functions import split, col, from_json
 import os
 import multiprocessing
 
@@ -25,5 +28,27 @@ data_df = session \
     .option('startingOffsets', 'earliest') \
     .load()
 
-data_df = data_df.selectExpr("CAST(value as STRING)")
-data_df.writeStream.outputMode('append').format('console').start().awaitTermination()
+data_df.printSchema()
+
+Schema = StructType([
+    StructField('category', StringType(), True),
+    StructField('index', IntegerType(), True),
+    StructField('unique_id', StringType(), True),
+    StructField('title', StringType(), True),
+    StructField('description', StringType(), True),
+    StructField('follower_count', StringType(), True),
+    StructField('tag_list', StringType(), True),
+    StructField('is_image_or_video', StringType(), True),
+    StructField('image_src', StringType(), True),
+    StructField('downloaded', IntegerType(), True),
+    StructField('save_location', StringType(), True)
+])
+
+df = data_df.selectExpr("CAST(value as STRING)",'timestamp')
+df = df.withColumn('value', from_json(df['value'], Schema)).select(col('value.*'),col('timestamp'))
+df = df.drop('unique_id','downloaded','save_location').withColumnRenamed('index','site_index') # drop unwanted columns
+df = df.select('*',split(col('tag_list'),',').alias('tags')).drop('tag_list')
+
+df.writeStream.outputMode('append').format('console').start().awaitTermination()
+
+df.show(10)
